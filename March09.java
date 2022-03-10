28f8770b12625117d960b929bafd6104c0b2b101
Nesar202 <101302588+Nesar202@users.noreply.github.com>
2022-03-09 20:56:39 -0600

    Add files via upload

--- March09.java
+++ March09.java
@@ -11,8 +11,9 @@ public class March09 {
 		System.out.println("Imput AC Numbers: ");
 		int AC_Number = s.nextInt();
 		for(int i=0; i<AC_Number; i++) {
-			System.out.println("Enter AC type: ");
+			
 			String AC_Type=s.nextLine();
+			System.out.println("Enter AC type: ");
 		}
 		System.out.println("Number of Inverter AC is 6");
 		System.out.println("Number of Inverter AC is 4");
