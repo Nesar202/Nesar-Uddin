f653c3a25c080e759394b5528b14adf12dec77aa
Nesar202 <101302588+Nesar202@users.noreply.github.com>
2022-03-09 20:39:05 -0600

    Add files via upload

---
+++ March09.java
@@ -0,0 +1,22 @@
+package ForLoop;
+
+import java.util.Scanner;
+
+public class March09 {
+	
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Imput AC Numbers: ");
+		int AC_Number = s.nextInt();
+		int I_AC_Number=0;
+		int NI_AC_Number=0;
+		String AC_Number1=s.nextLine();
+		for(int i=0; i<AC_Number; i++) {
+			System.out.println("Enter AC type: ");
+			String AC_Type=s.nextLine();
+		}
+	}
+
+}
