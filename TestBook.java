add3e872e83a28fa2a3027d57f66952f0b7241f5
Nesar202 <101302588+Nesar202@users.noreply.github.com>
2022-03-16 20:44:40 -0500

    Add files via upload

--- TestBook.java
+++ TestBook.java
@@ -3,10 +3,19 @@ package Package1;
 public class TestBook extends Book {
 
 	public static void main(String[] args) {
+		
 		Book b = new Book();
-		b.setBookPrice(300);
+		
+		System.out.println("Enter the Book Name: ");
+		b.setBookName("Java");
+		System.out.println(b.getBookName());
+		
+		System.out.println("Enter the Book Price: ");
+		b.setBookPrice(500);
 		System.out.println(b.getBookPrice());
-		b.setAuthorName("History");
+		
+		System.out.println("Enter the Author Name: ");
+		b.setAuthorName("Einstein");
 		System.out.println(b.getAuthorName());
 			
 
