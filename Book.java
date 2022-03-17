46bf2f5c9f1222e17b2e8e629baec5ecc7b5fa91
Nesar202 <101302588+Nesar202@users.noreply.github.com>
2022-03-16 20:34:31 -0500

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,21 @@
+package Package1;
+
+public class Book {
+	private int bookPrice;
+	private String authorName;
+	
+	public int getBookPrice() {
+		return bookPrice;
+	}
+	public void setBookPrice(int bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+	public String getAuthorName() {
+		return authorName;
+	}
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+		
+
+}
